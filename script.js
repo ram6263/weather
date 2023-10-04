ce0657fd6a88014831f63eaee8b45b9a53432bb3
RAMNARAYAN SANODIYA <sanodiyaramnarayan92@gmail.com>
2023-10-04 18:58:53 +0530

    first commit

--- script.js
+++ script.js
@@ -34,6 +34,15 @@ submit.addEventListener("click", (e) => {
     getWeather(city.value)
 })
 
+//////hover div//////
+$(document).ready(function () {
+    $('.hover-div').hover(function () {
+        $('.hover-div').stop().fadeTo('fast', 0.3);
+        $(this).stop().fadeTo('fast', 1);
+    }, function () {
+        $('.hover-div').stop().fadeTo('fast', 1);
+    });
+});
 ////////////1//////////////////////
 const info = document.getElementById("d").innerHTML;
 const geWeather = (city) => {
